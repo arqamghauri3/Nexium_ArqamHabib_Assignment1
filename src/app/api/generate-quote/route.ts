178b628a98c42fc7cd63af8d097a7eef5f139728
Arqam Habib <arqamghauri3@gmail.com>
2025-07-03 23:02:26 +0500

    fix: Moved the api calling to a seperate route.

--- src/app/api/generate-quote/route.ts
+++ src/app/api/generate-quote/route.ts
@@ -3,8 +3,19 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
-    
-    return NextResponse.json({ message: "Hello, world!" });
+    try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
+          headers:{
+            'X-Api-Key': process.env.X_Api_Key
+          }
+        })
+  
+        return NextResponse.json(response.data)
+      } catch (error) {
+        console.log("Error", error);
+        
+      }
+  
 }
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,21 @@
 import Link from "next/link";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false)
 
 
   const generateQuote = async() => {
+    setIsDisabled(true)
     try {
-      const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
-        headers:{
-          'X-Api-Key': 'ZmXkl3O/OGYy8N1p2mIS8w==MrAffKqyQQN46BPn'
-        }
-      })
-      console.log(response.data[0].quote);
+      const response = await axios.get('/api/generate-quote')
       setQuote(response.data[0].quote)
       setAuthor(response.data[0].author)
-      
+      setIsDisabled(false)
 
       return response
     } catch (error) {
@@ -30,7 +28,6 @@ export default function HomePage() {
   };
 
   useEffect(()=>{
-    generateQuote()
     
   },[])
 
@@ -39,7 +36,9 @@ export default function HomePage() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Quote Generator</h1>
       <p className="text-xl font-semibold">{quote} - "{author}"</p>
-      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={generateQuote}>Generate Quote</button>
+      <Button className="bg-black  text-white p-2 rounded-md cursor-pointer" disabled={isDisabled} onClick={generateQuote}>
+        {isDisabled ? <p>Generating Quote</p> : <p>Generate Quote</p>}
+      </Button>
     </div>
   );
 }
